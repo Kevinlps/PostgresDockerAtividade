fd9ca9d84c4642efa237bbf3225eb1f7e6866c17
ifms <ifms@labinfo>
2022-05-18 20:05:38 -0400

    bjb

--- src/controller/ArticleContrller.ts
+++ src/controller/ArticleContrller.ts
@@ -11,19 +11,19 @@ export const saveArticle = async (article : Article)=>{
     const {id} = response[0]
 
     const {editors} = article
-    editors.forEach(async (e) => {
+    for(let e of editors) {
         await sql`
             insert into editor_artigo values (
                 ${e.id}, ${id}
             )
         `
-    })
+    }
 
 }
 
 export const findALLArticles = async() => {
     const response = await sql`
-        select a.id, a.titulo, a.conteudo, e.nome
+        select a.id as id_artigo, a.titulo, a.conteudo, e.nome as nome_editor
         from artigo a 
         inner join
         editor_artigo ea
